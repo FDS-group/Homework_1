56e5bd14b5a777c6473cb6bb0e503496d1be23bc
juan_mata <juan.mnaranjo@hotmail.com>
2020-10-28 23:30:08 +0100

    Implement 1d convolution for question 1 and test against 2d convolution

--- Filtering/gauss_module.py
+++ Filtering/gauss_module.py
@@ -1,7 +1,5 @@
 # import packages: numpy, math (you might need pi for gaussian functions)
 import numpy as np
-import math
-import matplotlib.pyplot as plt
 import scipy
 from scipy.signal import convolve2d as conv2
 
@@ -36,26 +34,16 @@ Output: smoothed image
 """
 
 
-def gaussianfilter(img, sigma, conv_1d=False):
-    if not conv_1d:
-        gx, x = gauss(sigma)
-        gx = gx.reshape(1, gx.shape[0])
-        gy = gx.reshape(gx.shape[1], gx.shape[0])
-        smooth_img = scipy.signal.convolve2d(img, gx * np.array(gy))
-    else:
-        # Perform convolution using 1d kernel filters
-        x_axis_convolution = []
-        for i in range(img.shape[0]):
-            x_axis_convolution.append(
-                conv2(img[i].reshape((1, img.shape[1])), gauss_kernel_1d.reshape((1, len_1d_kernel)))[0].tolist())
-        y_axis_convolution = []
-        for i in range(np.array(x_axis_convolution).shape[1]):
-            y_axis_convolution.append(
-                conv2(np.array(x_axis_convolution)[:, i].reshape((1, np.array(x_axis_convolution).shape[0])),
-                      gauss_kernel_1d.reshape((1, len_1d_kernel)))[0].tolist())
-        smooth_img = np.array(y_axis_convolution).T
+def gaussianfilter(img, sigma):
+
+    gx, x = gauss(sigma)
+    gx = gx.reshape(1, gx.shape[0])
+    gy = gx.reshape(gx.shape[1], gx.shape[0])
+    smooth_img_x = conv2(img, gx)
+    smooth_img = conv2(smooth_img_x, gy)
 
     return smooth_img
+
 
 """
 Gaussian derivative function taking as argument the standard deviation sigma
@@ -70,15 +58,22 @@ def gaussdx(sigma):
     return dx, x
 
 
-def gaussderiv(img, sigma):
-    dx, x = gaussdx(sigma)
-    dx = dx.reshape(1, dx.shape[0])
-    new_row = np.zeros((((dx.shape[1] - dx.shape[0]) // 2), dx.shape[1]))
-    new_matrix = np.concatenate((new_row, dx), axis=0)
-    new_matrix = np.concatenate((new_matrix, new_row), axis=0)
-    img= gaussianfilter(img, sigma)
-    imgDx = scipy.signal.convolve2d(img, new_matrix)
-    imgDy = scipy.signal.convolve2d(img, new_matrix.transpose())
+def gaussderiv(img, sigma, attempt=True):
+    if attempt:
+        dx, x = gaussdx(sigma)
+        dx = dx.reshape(1, dx.shape[0])
+        new_row = np.zeros((((dx.shape[1] - dx.shape[0]) // 2), dx.shape[1]))
+        new_matrix = np.concatenate((new_row, dx), axis=0)
+        new_matrix = np.concatenate((new_matrix, new_row), axis=0)
+        img = gaussianfilter(img, sigma)
+        imgDx = scipy.signal.convolve2d(img, new_matrix)
+        imgDy = scipy.signal.convolve2d(img, new_matrix.transpose())
+    else:
+        dx, x = gaussdx(sigma)
+        dx = dx.reshape(1, dx.shape[0])
+        dy = dx.reshape(dx.shape[1], dx.shape[0])
+        img = gaussianfilter(img, sigma)
+        imgDx = scipy.signal.convolve2d(img, dx)
+        imgDy = scipy.signal.convolve2d(img, np.array(dy))
 
     return imgDx, imgDy
-
